d475eba19c17be3e106c12dbd7d2baa1b4f1e123
null-a <null-a@users.noreply.github.com>
2019-04-26 13:59:39 +0100

    Improve example.

--- brmp/ex0.py
+++ brmp/ex0.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 from pyro.contrib.brm import brm, makecode
 from pyro.contrib.brm.formula import Formula, Group, _1
 from pyro.contrib.brm.design import makedata
-from pyro.contrib.brm.utils import print_marginals
+from pyro.contrib.brm.fit import print_marginals
 
 def f(x):
     eps = np.random.normal(0, 0.5, x.shape)
@@ -13,23 +13,51 @@ def f(x):
 
 xs = np.random.uniform(0, 5, [10])
 ys = f(xs)
+df = pd.DataFrame(dict(x=xs, y=ys))
 
 # plt.scatter(xs, ys, marker='x')
 # plt.show()
 
-df = pd.DataFrame(dict(x=xs, y=ys))
-
 # y ~ 1 + x
 formula = Formula('y', [_1, 'x'], [])
+fit = brm(formula, df)
 
-# data = makedata(formula, df)
-# for (k, v) in data.items():
-#     print('========================================')
-#     print(k)
-#     print('----------------------------------------')
-#     print(v)
+print_marginals(fit)
+# ==================================================
+# b
+# -- mean ------------------------------------------
+# tensor([-1.9719,  3.9674])
+# -- stddev ----------------------------------------
+# tensor([0.3830, 0.1312])
+# ==================================================
+# sigma
+# -- mean ------------------------------------------
+# tensor([0.4817])
+# -- stddev ----------------------------------------
+# tensor([0.1592])
 
-#code = makecode(formula, df)
-#print(code)
+print(fit.code)
+# def model(X, y_obs=None):
+#     assert type(X) == torch.Tensor
+#     N = X.shape[0]
+#     M = 2
+#     assert X.shape == (N, M)
+#     b = pyro.sample("b", dist.Cauchy(torch.zeros([2]), torch.ones([2])).to_event(1))
+#     mu = torch.mv(X, b)
+#     sigma = pyro.sample("sigma", dist.HalfCauchy(torch.tensor(3.0).expand([1])).to_event(1))
+#     with pyro.plate("obs", N):
+#         y = pyro.sample("y", dist.Normal(mu, sigma.expand(N)), obs=y_obs)
+#     return dict(b=b, sigma=sigma, y=y)
 
-print_marginals(brm(formula, df))
+print(fit.data)
+# {'X': tensor([[1.0000, 0.6350],
+#         [1.0000, 4.6728],
+#         [1.0000, 3.8269],
+#         [1.0000, 2.1532],
+#         [1.0000, 2.9952],
+#         [1.0000, 2.2982],
+#         [1.0000, 0.9645],
+#         [1.0000, 1.6291],
+#         [1.0000, 1.7237],
+#         [1.0000, 4.2023]]), 'y_obs': tensor([ 0.8886, 16.3859, 13.2107,  6.7207,  9.9708,  6.4037,  1.5546,  4.0902,
+#          5.1075, 15.1358])}
