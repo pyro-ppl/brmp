12dc17b96ab4249e36109808dc383393d5bee720
null-a <null-a@users.noreply.github.com>
2019-04-26 13:59:17 +0100

    Introduce a Fit class.

--- brmp/__init__.py
+++ brmp/__init__.py
@@ -2,6 +2,7 @@ from pyro.infer.mcmc import MCMC, NUTS
 
 from pyro.contrib.brm.codegen import genmodel, eval_model
 from pyro.contrib.brm.design import makedata, dfmetadata
+from pyro.contrib.brm.fit import Fit
 
 def makecode(formula, df):
     return genmodel(formula, dfmetadata(df))
@@ -11,4 +12,5 @@ def brm(formula, df):
     model = eval_model(code)
     data = makedata(formula, df)
     nuts_kernel = NUTS(model, jit_compile=False, adapt_step_size=True)
-    return MCMC(nuts_kernel, num_samples=500, warmup_steps=100).run(**data)
+    run = MCMC(nuts_kernel, num_samples=500, warmup_steps=100).run(**data)
+    return Fit(run, code, data)
--- brmp/fit.py
+++ brmp/fit.py
@@ -1,14 +1,20 @@
-def print_marginals(mcmc_run):
+from collections import namedtuple
+
+Fit = namedtuple('Fit', ['run', 'code', 'data'])
+
+def print_marginals(fit):
+    assert type(fit) == Fit
+
     # Looking at only the first trace is OK here because the models
     # described by lme4 have static structure.
-    trace = mcmc_run.exec_traces[0]
+    trace = fit.run.exec_traces[0]
 
     # Is there a more Pyronic (not poking in trace internals) way to
     # achieve this? Or is it safer the have `genmodel` return a list
     # of sample sites used by the models it generates?
     sample_sites = [k for k in trace.nodes.keys()
                     if trace.nodes[k]['type'] == 'sample' and not k in ['obs', 'y']]
-    marginal = mcmc_run.marginal(sample_sites)
+    marginal = fit.run.marginal(sample_sites)
     for name in sample_sites:
         print('==================================================')
         print(name)
