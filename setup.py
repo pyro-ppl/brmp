813df8e825b09b59004be2a5cb7f92141ba4d007
null-a <null-a@users.noreply.github.com>
2019-09-30 10:35:09 +0100

    Add some dependencies to setup.py.

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='brmp',
-      packages=find_packages())
+      packages=find_packages(),
+      install_requires=[
+        'pandas',
+        'pyro-ppl>=0.4.1',
+        'numpyro>=0.2.0',
+      ],
+      extras_require={
+        'test': ['pytest']
+      })
