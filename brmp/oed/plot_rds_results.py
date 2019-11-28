59039faa929b1e7c180ad3ffafd9e5c4f9f0fbb5
null-a <null-a@users.noreply.github.com>
2019-11-28 15:33:46 +0000

    Update RDS plot script.

--- brmp/oed/plot_rds_results.py
+++ brmp/oed/plot_rds_results.py
@@ -1,6 +1,5 @@
 import json
 import sys
-from collections import defaultdict
 
 from matplotlib import pyplot as plt
 
@@ -8,31 +7,18 @@ from matplotlib import pyplot as plt
 def main(fn):
     with open(fn, 'r') as f:
         data = json.load(f)
-
-    d = defaultdict(list)
-    for name, values in data.items():
-        for (M, bayes_factor) in values:
-            d[(M, name)].append(bayes_factor)
-
-    def adjust_name(name):
-        if name == 'oed_alt':
-            return 'alt'
-        else:
-            return name
+    keys = sorted(data.keys())
 
     plt.figure(figsize=(6.5, 4.5))
-    keys = [(M, name) for M, name in sorted(d.keys())]
-    plt.boxplot([d[k] for k in keys],
-                labels=['{}\nM={}'.format(adjust_name(name), M) for M, name in keys],
+    plt.boxplot([data[k] for k in keys],
+                labels=keys,
                 showfliers=False,
                 whis='range')
 
-    colours = {'oed': 'r', 'oed_alt': 'g', 'rand': 'b'}
     for i, k in enumerate(keys):
-        M, name = k
-        plt.scatter([i+1 for _ in d[k]], d[k], c=colours[name], alpha=0.2, marker='.')
+        plt.scatter([i+1 for _ in data[k]], data[k], alpha=0.2, marker='.')
 
-    plt.xticks(fontsize=8)
+    plt.xticks(fontsize=7, rotation=10)
     plt.ylabel('final Bayes factor')
     plt.title('OED - real data simulation on synthetic data')
     plt.tight_layout()
