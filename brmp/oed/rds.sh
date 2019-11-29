c5fc29bdf77f8778f8a5e139ba880288460cef2f
null-a <null-a@users.noreply.github.com>
2019-11-29 15:26:31 +0000

    Remove duplicated argument.

--- brmp/oed/rds.sh
+++ brmp/oed/rds.sh
@@ -1,4 +1,4 @@
 for i in {1..20}
 do
-    python rds.py -m 2 --num-samples 6000 --num-epochs 200 -lr 0.0001 --weight-decay 0.5 --num-epochs 200 --opt-method SGD --interval-method adapt
+    python rds.py -m 2 --num-samples 6000 --num-epochs 200 -lr 0.0001 --weight-decay 0.5 --opt-method SGD --interval-method adapt
 done
